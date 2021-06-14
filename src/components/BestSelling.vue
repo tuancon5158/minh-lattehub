124ae37f33d8919613c16be978ccbcda6ad584f8
minhpt6 <minhtp@lattehub.com>
2021-06-14 18:07:00 +0700

    add new

---
+++ src/components/BestSelling.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div class="title"><h2 >Best Selling</h2></div>
+        <div><button>VIEW MORE</button></div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang='css'> 
+
+</style>
